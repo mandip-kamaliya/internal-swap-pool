fc432f5b480d9a975f5bec873171443e4fbadcb7
mandip kamaliya <mandipkamaliya7@gmail.com>
2025-11-03 19:10:41 +0530

    added distributed fees function

--- src/internalSwapPool.sol
+++ src/internalSwapPool.sol
@@ -1,42 +1,43 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.22;
- 
-import {SwapMath} from '@uniswap/v4-core/src/libraries/SwapMath.sol';
-import {Hooks, IHooks} from '@uniswap/v4-core/src/libraries/Hooks.sol';
-import {BalanceDelta} from '@uniswap/v4-core/src/types/BalanceDelta.sol';
-import {Currency, CurrencyLibrary} from '@uniswap/v4-core/src/types/Currency.sol';
-import {PoolId, PoolIdLibrary} from '@uniswap/v4-core/src/types/PoolId.sol';
-import {PoolKey} from '@uniswap/v4-core/src/types/PoolKey.sol';
-import {BeforeSwapDelta, toBeforeSwapDelta} from '@uniswap/v4-core/src/types/BeforeSwapDelta.sol';
-import {ModifyLiquidityParams, SwapParams} from '@uniswap/v4-core/src/types/PoolOperation.sol';
-import {StateLibrary} from '@uniswap/v4-core/src/libraries/StateLibrary.sol';
-import {CurrencySettler} from '@uniswap/v4-core/test/utils/CurrencySettler.sol';
- 
-import {IERC20Minimal} from '@uniswap/v4-core/src/interfaces/external/IERC20Minimal.sol';
-import {IPoolManager} from '@uniswap/v4-core/src/interfaces/IPoolManager.sol';
- 
-import {BaseHook} from 'v4-periphery/src/utils/BaseHook.sol';
 
-contract internalSwapPool is BaseHook{
+import {SwapMath} from "@uniswap/v4-core/src/libraries/SwapMath.sol";
+import {Hooks, IHooks} from "@uniswap/v4-core/src/libraries/Hooks.sol";
+import {BalanceDelta} from "@uniswap/v4-core/src/types/BalanceDelta.sol";
+import {Currency, CurrencyLibrary} from "@uniswap/v4-core/src/types/Currency.sol";
+import {PoolId, PoolIdLibrary} from "@uniswap/v4-core/src/types/PoolId.sol";
+import {PoolKey} from "@uniswap/v4-core/src/types/PoolKey.sol";
+import {BeforeSwapDelta, toBeforeSwapDelta} from "@uniswap/v4-core/src/types/BeforeSwapDelta.sol";
+import {ModifyLiquidityParams, SwapParams} from "@uniswap/v4-core/src/types/PoolOperation.sol";
+import {StateLibrary} from "@uniswap/v4-core/src/libraries/StateLibrary.sol";
+import {CurrencySettler} from "@uniswap/v4-core/test/utils/CurrencySettler.sol";
+
+import {IERC20Minimal} from "@uniswap/v4-core/src/interfaces/external/IERC20Minimal.sol";
+import {IPoolManager} from "@uniswap/v4-core/src/interfaces/IPoolManager.sol";
+
+import {BaseHook} from "v4-periphery/src/utils/BaseHook.sol";
+
+contract internalSwapPool is BaseHook {
     using CurrencyLibrary for Currency;
     using CurrencySettler for Currency;
     using PoolIdLibrary for PoolKey;
     using StateLibrary for IPoolManager;
 
-    uint public constant DONATE_THRESHOLD_MINIUM = 0.01 ether;
+    uint256 public constant DONATE_THRESHOLD_MINIUM = 0.01 ether;
 
     address public immutable nativeToken;
 
     struct ClaimableFees {
-        uint amount0;
-        uint amount1;
+        uint256 amount0;
+        uint256 amount1;
     }
 
-    mapping (PoolId _poolId => ClaimableFees _fees) internal _poolFees;
-     constructor (address _poolManager, address _nativeToken) BaseHook(IPoolManager(_poolManager)) {
+    mapping(PoolId _poolId => ClaimableFees _fees) internal _poolFees;
+
+    constructor(address _poolManager, address _nativeToken) BaseHook(IPoolManager(_poolManager)) {
         nativeToken = _nativeToken;
     }
- 
+
     /**
      * Provides the {ClaimableFees} for a pool.
      *
@@ -47,7 +48,7 @@ contract internalSwapPool is BaseHook{
     function poolFees(PoolKey calldata _poolKey) public view returns (ClaimableFees memory) {
         return _poolFees[_poolKey.toId()];
     }
- 
+
     /**
      * When fees are collected against a collection it is sent as ETH in a payable
      * transaction to this function. This then handles the distribution of the
@@ -62,11 +63,11 @@ contract internalSwapPool is BaseHook{
      * @param _amount0 The amount of currency0 to deposit
      * @param _amount1 The amount of currency1 to deposit
      */
-    function depositFees(PoolKey calldata _poolKey, uint _amount0, uint _amount1) public {
+    function depositFees(PoolKey calldata _poolKey, uint256 _amount0, uint256 _amount1) public {
         _poolFees[_poolKey.toId()].amount0 += _amount0;
         _poolFees[_poolKey.toId()].amount1 += _amount1;
     }
- 
+
     /**
      * Before a swap is made, we pull in the dynamic pool fee that we have set to ensure it is
      * applied to the tx.
@@ -84,10 +85,14 @@ contract internalSwapPool is BaseHook{
      * @return beforeSwapDelta_ The hook's delta in specified and unspecified currencies. Positive: the hook is owed/took currency, negative: the hook owes/sent currency
      * @return swapFee_ The percentage fee applied to our swap
      */
-    function _beforeSwap(address sender, PoolKey calldata key, SwapParams calldata params, bytes calldata hookData) internal override returns (bytes4 selector_, BeforeSwapDelta beforeSwapDelta_, uint24 swapFee_) {
+    function _beforeSwap(address sender, PoolKey calldata key, SwapParams calldata params, bytes calldata hookData)
+        internal
+        override
+        returns (bytes4 selector_, BeforeSwapDelta beforeSwapDelta_, uint24 swapFee_)
+    {
         selector_ = IHooks.beforeSwap.selector;
     }
- 
+
     /**
      * Once a swap has been made, we distribute fees to our LPs and emit our price update event.
      *
@@ -100,10 +105,16 @@ contract internalSwapPool is BaseHook{
      * @return selector_ The function selector for the hook
      * @return hookDeltaUnspecified_ The hook's delta in unspecified currency. Positive: the hook is owed/took currency, negative: the hook owes/sent currency
      */
-    function _afterSwap(address sender, PoolKey calldata key, SwapParams calldata params, BalanceDelta delta, bytes calldata hookData) internal override returns (bytes4 selector_, int128 hookDeltaUnspecified_) {
+    function _afterSwap(
+        address sender,
+        PoolKey calldata key,
+        SwapParams calldata params,
+        BalanceDelta delta,
+        bytes calldata hookData
+    ) internal override returns (bytes4 selector_, int128 hookDeltaUnspecified_) {
         selector_ = IHooks.afterSwap.selector;
     }
- 
+
     /**
      * Takes a collection address and, if there is sufficient fees available to
      * claim, will call the `donate` function against the mapped Uniswap V4 pool.
@@ -114,9 +125,24 @@ contract internalSwapPool is BaseHook{
      * @param _poolKey The PoolKey reference that will have fees distributed
      */
     function _distributeFees(PoolKey calldata _poolKey) internal {
-        //
+        // Get the amount of the native token available to donate
+        PoolId poolId = _poolKey.toId();
+        uint256 donateAmount = _poolFees[poolId].amount0;
+
+        // Ensure that the collection has sufficient fees available
+        if (donateAmount < DONATE_THRESHOLD_MIN) {
+            return;
+        }
+
+        // Make our donation to the pool
+        BalanceDelta delta = poolManager.donate(_poolKey, donateAmount, 0, "");
+
+        // @todo We need to settle tokens here
+
+        // Reduce our available fees
+        _poolFees[poolId].amount0 -= donateAmount;
     }
- 
+
     /**
      * This function defines the hooks that are required, and also importantly those which are
      * not, by our contract. This output determines the contract address that the deployment
@@ -140,7 +166,4 @@ contract internalSwapPool is BaseHook{
             afterRemoveLiquidityReturnDelta: false
         });
     }
- 
 }
-
-
